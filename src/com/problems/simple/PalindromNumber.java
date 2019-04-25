b841e642cc45c4c477aa2add84bb6a4707d36273
Samuel Parmar <samuel.parmar@gmail.com>
2019-04-24 22:29:58 -0700

    First Commit with simple problems

---
+++ src/com/problems/simple/PalindromNumber.java
@@ -0,0 +1,33 @@
+package com.problems.simple;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ *	Example 1: Input: 121  Output: true
+ *  Example 2: Input: -121   Output: false (it reads -121. From right to left, it becomes 121-)
+ * @author samuel
+ *
+ */
+public class PalindromNumber {
+	
+	public boolean isPalindrome(int x) {
+        int reverse = 0;
+        int orig = x;
+        if (x < 0) return false;
+        if (x % 10 == x) return true; //single digit 
+        while (x / 10 != 0 ){
+            reverse = reverse*10 + x % 10 ;
+            x = x / 10;
+        }
+        reverse = reverse*10 + x % 10 ; //do it for the last digit
+        return orig == reverse; 
+    }
+	
+	public static void main(String[] args) {
+		assertTrue("it should be true",new PalindromNumber().isPalindrome(0));
+		assertTrue("it should be true",new PalindromNumber().isPalindrome(101));
+		assertFalse("it should be true",new PalindromNumber().isPalindrome(-101));
+		assertFalse("it should be true",new PalindromNumber().isPalindrome(10));
+	}
+}
---
+++ src/com/problems/simple/ValidParenthesis.java
@@ -0,0 +1,76 @@
+package com.problems.simple;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * @author samuel
+ *
+ */
+
+public class ValidParenthesis {
+	public boolean isValid(String s) {
+		if (s == null || s.isEmpty()) return true;
+        char[] charArr= new char[s.length()];
+        int index = 0;
+        int arrIndex = -1 ;
+        while (index < s.length()){
+        	char c = s.charAt(index++);
+        	switch(c){
+        	case '{':
+        	case '[':
+        	case '(':
+        		charArr[++arrIndex] = c;
+        		break;
+        	case '}':
+        		if ( arrIndex < 0 || charArr[arrIndex] != '{')
+        			return false;
+        		else{
+        			arrIndex--;
+        			break;
+        		}
+        	case ']':
+        		if ( arrIndex < 0 || charArr[arrIndex] != '[')
+        			return false;
+        		else{
+        			arrIndex--;
+        			break;
+        		}
+        	case ')':
+        		if ( arrIndex < 0 || charArr[arrIndex] != '(')
+        			return false;
+        		else{
+        			arrIndex--;
+        			break;
+        		}
+        	default :
+        		return false;
+        	}
+        }
+        if (arrIndex != -1) return false;
+        return true;
+    }
+	
+	public static void main(String[] args) {
+		assertTrue("it should be true",new ValidParenthesis().isValid(""));
+		assertFalse("it should be true",new ValidParenthesis().isValid("]"));
+		assertFalse("it should be true",new ValidParenthesis().isValid("{["));
+		assertTrue("it should be true",new ValidParenthesis().isValid("{}[]()"));
+	}
+}
